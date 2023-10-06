02f4efcff9a272c19297f710ca8ef6121dd8cc28
BearmanAdmin <william.k97430@gmail.com>
2023-10-06 11:49:58 +0200

    add prometheus

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,46 @@ import swaggerUi from 'swagger-ui-express'
 import { swaggerDocument } from './utils/swagger/swagger'
 import { corsApp } from './utils/cors/corsApp'
 import SessionController from "./controllers/session";
-const cors = require('cors');
+import cors from 'cors';
+import prometheus from 'prom-client';
 
 const port: number = 3001
 const app = express()
 const httpServer: Express = require('http').createServer(app)
 const io = require('socket.io')(httpServer, corsApp)
 
-app.use(cors({origin:"*"}))
+// Créez un registre Prometheus
+const register = new prometheus.Registry();
+// Créez une métrique personnalisée (Gauge)
+const customMetric = new prometheus.Gauge({
+    name: 'my_custom_metric',
+    help: 'A custom metric for my application',
+    registers: [register], // Associez la métrique au registre
+});
+// Collect default metrics
+prometheus.collectDefaultMetrics({ register });
+app.get('/metrics', async (req, res) => {
+    try {
+        res.set('Content-Type', register.contentType);
+        const metrics = await register.metrics();
+        res.end(metrics);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Erreur lors de la génération des métriques Prometheus');
+    }
+});
+
+
+
+
+
+app.use(cors(corsApp))
 app.use(express.json())
 app.use('/docs', swaggerUi.serve, swaggerDocument)
 app.use('/session', SessionController)
-//app.use(require("/src/controllers/socket")(io))
 require("./controllers/socket")(io)
 
 
-httpServer.listen(port, () => { console.log(`listening on port ${port}`) })
+httpServer.listen(port, () => { console.log(`listening on port ${port} ${process.argv[2]}`) })
 
 module.exports.ioobject = io;
--- src/utils/cors/corsApp.ts
+++ src/utils/cors/corsApp.ts
@@ -1,11 +1,10 @@
 import cors from 'cors'
+
 require('dotenv').config();
 
 export const corsApp = {
-  cors: {
     origin: '*',//`http://${process.env.CLIENT_HOST}`,
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type, Authorization'],
     credentials: true
-  }
 }
